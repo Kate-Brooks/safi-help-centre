57a1f1fac2758cce65ed72e70b2c2c348f56dead
Kate Brooks <kate@safi.co>
2026-06-04 16:42:38 +0200

    Update html2pdf.d.ts

--- src/types/html2pdf.d.ts
+++ src/types/html2pdf.d.ts
@@ -3,8 +3,11 @@ declare module 'html2pdf.js' {
   interface Html2PdfWorker {
     set(opt: Record<string, unknown>): Html2PdfWorker;
     from(element: HTMLElement | string): Html2PdfWorker;
+    toPdf(): Html2PdfWorker;
+    get(key: string): Html2PdfWorker;
     save(filename?: string): Promise<void>;
-    then(onFulfilled: () => void): Html2PdfWorker;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    then(onFulfilled: (value: any) => void): Html2PdfWorker;
   }
   function html2pdf(): Html2PdfWorker;
   export default html2pdf;
